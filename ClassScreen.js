12ea3bf8436243a4b11850e5e7707f35c5a1f193
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-02-15 19:38:29 +0200

    INIT: list of users' classes
    flatlist showing users' classes as a homescreen with mock data

--- ClassScreen.js
+++ ClassScreen.js
@@ -1,18 +1,45 @@
 import styles from "./styles";
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, StyleSheet, FlatList } from "react-native";
+import Card from "./utils/Card";
+
+const renderCard = ({ item }) => <Card>{item.name}</Card>;
+
+const data = [
+  { name: "card 1", key: 1 },
+  { name: "card 2", key: 2 },
+  { name: "card 3", key: 3 },
+  { name: "card 4", key: 4 },
+  { name: "card 5", key: 5 },
+];
 
 function ClassScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      {/* <View> */}
-      <Text>home</Text>
-      <Button
-        title="Go to profile"
-        // onPress={() => navigation.navigate("Profile", { name: "Jahne" })}
-        onPress={() => console.log("hi")}
+    <View
+      style={{
+        ...styles.darkBackground,
+        ...classStyle.container,
+      }}
+    >
+      <FlatList
+        style={classStyle.cardContainer}
+        data={data}
+        renderItem={renderCard}
       />
+      {/* <Button title="Go to profile" onPress={() => console.log("hi")} /> */}
     </View>
   );
 }
+
+const classStyle = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+  },
+  cardContainer: {
+    flex: 1,
+    // backgroundColor: "pink",
+    padding: 5,
+  },
+});
 
 export default ClassScreen;
