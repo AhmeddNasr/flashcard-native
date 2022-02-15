7759f56a75d52e014c366e471352df637989db0f
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-02-15 22:16:20 +0200

    FEAT: cards onpress direct to their preview screen
    Cards on press direct to empty screen while passing their id as a prop to be later used to retrieve card data from DB

--- ClassScreen.js
+++ ClassScreen.js
@@ -2,14 +2,28 @@ import styles from "./styles";
 import { Text, View, Button, StyleSheet, FlatList } from "react-native";
 import Card from "./utils/Card";
 
-const renderCard = ({ item }) => <Card>{item.name}</Card>;
+const renderCard = ({ item }, navigation) => (
+  <View style={{ margin: 10 }}>
+    <Card
+      // onPress={() => {
+      //   console.log(navigation);
+      //   navigation.navigate("Profile", { id: item.key });
+      // }}
+      id={item.id}
+      navigation={navigation}
+      title="hi"
+    >
+      {item.name}
+    </Card>
+  </View>
+);
 
 const data = [
-  { name: "card 1", key: 1 },
-  { name: "card 2", key: 2 },
-  { name: "card 3", key: 3 },
-  { name: "card 4", key: 4 },
-  { name: "card 5", key: 5 },
+  { name: "card 1", key: 1, id: 1 },
+  { name: "card 2", key: 2, id: 2 },
+  { name: "card 3", key: 3, id: 3 },
+  { name: "card 4", key: 4, id: 4 },
+  { name: "card 5", key: 5, id: 5 },
 ];
 
 function ClassScreen({ navigation }) {
@@ -23,7 +37,8 @@ function ClassScreen({ navigation }) {
       <FlatList
         style={classStyle.cardContainer}
         data={data}
-        renderItem={renderCard}
+        renderItem={(item) => renderCard(item, navigation)}
+        // showsVerticalScrollIndicator={false}
       />
       {/* <Button title="Go to profile" onPress={() => console.log("hi")} /> */}
     </View>
@@ -32,8 +47,8 @@ function ClassScreen({ navigation }) {
 
 const classStyle = StyleSheet.create({
   container: {
-    padding: 20,
     flex: 1,
+    padding: 10,
   },
   cardContainer: {
     flex: 1,
