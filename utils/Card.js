151df485660da2e9211a961bb9e4a575d9f2a03f
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-02-18 03:17:47 +0200

    FEAT: Cards onPress and title prop
    Cards onPress directs to preview screen (tbm) passing id as prop.
    Cards accept a title prop to be used for displaying title of card.

--- utils/Card.js
+++ utils/Card.js
@@ -2,10 +2,23 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import theme from "../theme";
 
 function Card(props) {
+  // console.log(props);
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => {
+        props.navigation.navigate("Profile", { id: props.id });
+      }}
+    >
       <View style={cardStyle.container}>
-        <Text>{props.children}</Text>
+        <View>
+          <Text style={{ ...cardStyle.text, fontSize: theme.FONT_SIZE_LARGE }}>
+            {props.title}
+          </Text>
+        </View>
+        <Text style={{ ...cardStyle.text, fontStyle: "italic" }}>
+          {props.children}
+        </Text>
       </View>
     </TouchableOpacity>
   );
@@ -14,11 +27,14 @@ function Card(props) {
 const cardStyle = StyleSheet.create({
   container: {
     backgroundColor: theme.PRIMARY_COLOR,
-    // maxWidth: 250,
-    // maxHeight: 200,
-    borderRadius: 15,
-    flex: 1,
-    // alignItems: "center",
+    borderRadius: 6,
+    maxWidth: 400,
+    padding: 20,
+    // margin: 5,
+    height: 150,
+  },
+  text: {
+    color: theme.TEXT_COLOR,
   },
 });
 
