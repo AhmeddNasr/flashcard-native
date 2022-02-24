4cd5a3966d680eec80b4845399e51adbe1651bdf
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-02-24 07:51:14 +0200

    FEAT: Allow capturing photo and log photo uri
    Capture photo button calls takePictureAsync and logs photo uri

--- CameraScreen.js
+++ CameraScreen.js
@@ -1,33 +1,57 @@
 import { Camera } from "expo-camera";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
 import theme from "./theme";
+import { Ionicons } from "@expo/vector-icons";
+import { useRef } from "react";
 
 function CameraScreen() {
+  const camera = useRef(null);
+  const snap = () => {
+    const options = { quality: 0.5, base64: true, skipProcessing: true };
+    if (camera.current) {
+      camera.current
+        .takePictureAsync(options)
+        .then((result) => console.log(result.uri))
+        .catch((err) => console.log(err));
+      console.log("success");
+    }
+  };
   return (
     <View style={CameraStyles.container}>
-      <Camera style={CameraStyles.camera} />
-      {/* <View stlye={CameraStyles.cameraControls}>
-        <Text>Close camera</Text>
-      </View> */}
+      <View style={CameraStyles.container}>
+        <Camera style={CameraStyles.camera} ref={camera} />
+      </View>
+      <View stlye={CameraStyles.controlsContainer}>
+        <TouchableOpacity onPress={() => snap()}>
+          <Ionicons
+            style={{
+              fontSize: theme.FONT_SIZE_LARGE_ICON,
+              color: theme.TEXT_COLOR,
+            }}
+            name="camera"
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const CameraStyles = StyleSheet.create({
   container: {
+    backgroundColor: theme.BACKGROUND_COLOR,
     flex: 1,
-    // width: "100%",
   },
   camera: {
     flex: 1,
-    width: "100%",
+    flexGrow: 1,
+    // flexGrow: 1,
+    // flexBasis: 50,
   },
-  // cameraControls: {
-  //   flex: 1,
-  //   height: 100,
-  //   width: "100%",
-  //   backgroundColor: theme.PRIMARY_COLOR,
-  //   padding: 10,
-  // },
+  controlsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    flexGrow: 1,
+    paddingBottom: 5,
+  },
 });
 export default CameraScreen;
