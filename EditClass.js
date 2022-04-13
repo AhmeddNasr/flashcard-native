767b1d0032dc6c64cd54990ee50979a97e741ebe
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-04-13 05:02:26 +0200

    Create EditClass.js

---
+++ EditClass.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import { Text, View } from "react-native";
+import theme from "./theme";
+
+export default function EditClass() {
+  return (
+    <View>
+      <Text style={{ color: theme.TEXT_COLOR }}>Edit Class!</Text>
+    </View>
+  );
+}
