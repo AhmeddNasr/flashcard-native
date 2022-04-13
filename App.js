e2c28a09f3f079e24c0b269de1aa72762cff3f94
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-04-13 05:00:26 +0200

    FEAT: initialize database tables when app loads

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { Text } from "react-native";
+import { useEffect } from "react";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import {
   createStackNavigator,
@@ -9,16 +9,54 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 import MyAppBar from "./MyAppBar";
 // import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
-import ClassScreen from "./ClassScreen";
+import HomeScreen from "./HomeScreen";
 import styles from "./styles";
-import ProfileScreen from "./ProfileScreen";
+// import Class from "./ClassScreen";
 import theme from "./theme";
 import CreateClass from "./CreateClass";
 import CameraScreen from "./CameraScreen";
+import Test from "./Test";
+import CropView from "./CropView";
+import ClassScreen from "./ClassScreen";
+import * as SQLite from "expo-sqlite";
+import EditClass from "./EditClass";
+
+const db = SQLite.openDatabase("db.db");
 
 const Stack = createStackNavigator();
-
 export default function App() {
+  //initialize db tables
+  useEffect(() => {
+    console.log("App.js useEffect ran :)");
+    db.exec(
+      [{ sql: "PRAGMA foreign_keys = ON;", args: [] }],
+      false,
+      (error, resultSet) => {}
+    );
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS classes (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT)",
+        [],
+        (txObj, resultSet) => {
+          // console.log(resultSet);
+        },
+        (txObj, error) => {
+          console.log(error);
+        }
+      );
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS cards (id INTEGER PRIMARY KEY AUTOINCREMENT, class_id INTEGER REFERENCES classes(id), question_text TEXT, question_image TEXT, answer_text TEXT, answer_image TEXT)",
+        [],
+        (txObj, resultSet) => {
+          // console.log(resultSet);
+        },
+        (txObj, error) => {
+          console.log("error :", error);
+        }
+      );
+    });
+  }, []);
+
   const MyTheme = {
     ...DefaultTheme,
     colors: {
@@ -26,6 +64,7 @@ export default function App() {
       background: theme.BACKGROUND_COLOR,
     },
   };
+
   // return null;
   return (
     <NavigationContainer theme={MyTheme}>
@@ -33,12 +72,15 @@ export default function App() {
         // theme={MyTheme}
         screenOptions={{
           header: (props) => <MyAppBar {...props} />,
-          ...TransitionPresets.SlideFromRightIOS,
+          // ...TransitionPresets.SlideFromRightIOS,
+          headerMode: "screen",
         }}
       >
         <Stack.Screen name="Create" component={CreateClass} />
-        <Stack.Screen name="Home" component={ClassScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Edit" component={EditClass} />
+        <Stack.Screen name="Crop" component={CropView} />
+        <Stack.Screen name="Class" component={ClassScreen} />
         <Stack.Screen name="Camera" component={CameraScreen} />
       </Stack.Navigator>
       <StatusBar style="light" />
