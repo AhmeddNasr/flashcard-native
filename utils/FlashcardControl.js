6ed76a5375fd1c6ec76a3fed9edfc173a9e725bf
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-04-23 07:45:05 +0200

    FIX: removed animated opacity value
    removed animated opacity values due to buggy behavior, and used pressable style prop instead

--- utils/FlashcardControl.js
+++ utils/FlashcardControl.js
@@ -1,36 +1,21 @@
-import { Pressable, StyleSheet, Text, View, Animated } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import theme from "../theme";
 
 export default function FlashcardControl(props) {
-  const animated = new Animated.Value(1);
-
-  const fadeIn = () => {
-    Animated.timing(animated, {
-      toValue: 0.5,
-      duration: 100,
-      useNativeDriver: true,
-    }).start();
-  };
-  const fadeOut = () => {
-    Animated.timing(animated, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
-
   return (
     <Pressable
       onPress={() => props.action()}
-      onPressIn={fadeIn}
-      onPressOut={fadeOut}
+      style={({ pressed }) => [
+        {
+          opacity: pressed ? 0.3 : 1,
+          padding: 20,
+        },
+      ]}
     >
-      <Animated.View style={{ opacity: animated }}>
-        <View style={styles.container}>
-          <Text style={styles.title}>{props.title}</Text>
-          {props.children}
-        </View>
-      </Animated.View>
+      <View style={styles.container}>
+        <Text style={styles.title}>{props.title}</Text>
+        {props.children}
+      </View>
     </Pressable>
   );
 }
