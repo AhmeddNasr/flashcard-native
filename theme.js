7100af94a3d9f4c9bbc4bd314747c27662a6e103
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-04-23 06:31:02 +0200

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,9 +1,10 @@
 export default {
   TEXT_COLOR: "rgba(245, 246, 247, 1)",
   TEXT_COLOR_OPACITY: "rgba(245, 246, 247, .7)",
+  TEXT_COLOR_VERY_OPACITY: "rgba(245, 246, 247, .5)",
   // rgba(111, 202, 186, 1)
   BACKGROUND_COLOR: "#18191A",
-  PRIMARY_COLOR: "rgba(92, 99,216, 0.9)",
+  PRIMARY_COLOR: "rgba(92, 99,216, 1)",
   // ACCENT_COLOR: "#282A36",
   ACCENT_COLOR: "rgba(111, 202, 186, 1)",
   FONT_SIZE_SMALL: 12,
@@ -12,4 +13,5 @@ export default {
   FONT_SIZE_VERYLARGE: 20,
   FONT_SIZE_ICON: 26,
   FONT_SIZE_LARGE_ICON: 38,
+  FONT_SIZE_FLASHCARD_CONTROL: 40,
 };
