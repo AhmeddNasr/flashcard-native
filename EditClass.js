888b3e3b4097134aff882a86fafb92be2005a327
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-05-07 18:00:47 +0200

    STYLE: changed edit class design
    changed edit class design to be more simple and cleaner

--- EditClass.js
+++ EditClass.js
@@ -10,16 +10,17 @@ import {
 import theme from "./theme";
 import * as SQLite from "expo-sqlite";
 import { useFormik, FormikProvider, FieldArray, FastField } from "formik";
-import { Button } from "@rneui/themed";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import InputWithImage from "./utils/InputWithImage";
 import * as Yup from "yup";
+import FloatingActionButton from "./utils/FloatingActionButton";
 
 export default function EditClass({ route, navigation }) {
   const [cardsData, setCardsData] = useState([{ id: -1 }]);
   const [classData, setClassData] = useState([]);
   const [submitting, setSubmitting] = useState(false);
   const [listReady, setListReady] = useState(false);
+  const [deleting, setDeleting] = useState(true);
   const db = SQLite.openDatabase("db.db");
 
   const latestId = useRef(-2);
@@ -142,7 +143,7 @@ export default function EditClass({ route, navigation }) {
   });
 
   const formik = useFormik({
-    validateOnChange: false,
+    validateOnChange: true,
     validateOnBlur: true,
     initialValues: {
       name: classData.name,
@@ -162,144 +163,150 @@ export default function EditClass({ route, navigation }) {
   }
 
   return (
-    <KeyboardAwareScrollView
-      extraHeight={40}
-      extraScrollHeight={40}
-      scrollEnabled
-      enableAutomaticScroll
-      enableOnAndroid
-      keyboardShouldPersistTaps="handled"
-    >
-      <View style={styles.container}>
-        {/* form */}
-        <FormikProvider value={formik}>
-          {/* class info section */}
-          <View style={styles.header}>
-            <Text style={styles.header_text}>Class Info</Text>
-          </View>
-          {/* TODO DRY */}
-          <FastField
-            component={TextInput}
-            style={
-              formik.errors.name
-                ? { ...styles.input, borderColor: theme.SECONDARY_COLOR }
-                : styles.input
-            }
-            onChangeText={formik.handleChange("name")}
-            onBlur={formik.handleBlur("name")}
-            value={formik.values.name}
-            placeholder="Class Name"
-            placeholderTextColor={theme.TEXT_COLOR_OPACITY}
-          />
-          <Text
-            style={
-              formik.errors.name
-                ? { ...styles.input_hint, color: theme.SECONDARY_COLOR }
-                : styles.input_hint
-            }
-          >
-            {formik.errors.name ?? "Class Name"}
-          </Text>
-          <FastField
-            component={TextInput}
-            style={styles.input}
-            onChangeText={formik.handleChange("description")}
-            onBlur={formik.handleBlur("description")}
-            value={formik.values.description}
-            placeholder="Class Description"
-            placeholderTextColor={theme.TEXT_COLOR_OPACITY}
-            multiline
-          />
-          <Text
-            style={
-              formik.errors.description
-                ? { ...styles.input_hint, color: theme.SECONDARY_COLOR }
-                : styles.input_hint
-            }
-          >
-            {formik.errors.description ?? "Class Description"}
-          </Text>
-          {/* Cards section */}
-          <View style={styles.header}>
-            <Text style={styles.header_text}>
-              Cards ({formik.values.cards.length})
+    <View style={{ flex: 1 }}>
+      <KeyboardAwareScrollView
+        extraHeight={60}
+        extraScrollHeight={60}
+        scrollEnabled
+        enableOnAndroid
+        enableAutomaticScroll={false}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={styles.container}>
+          {/* form */}
+          <FormikProvider value={formik}>
+            {/* class info section */}
+            <View style={styles.header}>
+              <Text style={styles.header_text}>Class Info</Text>
+            </View>
+            {/* TODO DRY */}
+            <FastField
+              component={TextInput}
+              style={
+                formik.errors.name
+                  ? { ...styles.input, borderColor: theme.SECONDARY_COLOR }
+                  : styles.input
+              }
+              onChangeText={formik.handleChange("name")}
+              onBlur={formik.handleBlur("name")}
+              value={formik.values.name}
+              placeholder="Class Name"
+              placeholderTextColor={theme.TEXT_COLOR_OPACITY}
+            />
+            <Text
+              style={
+                formik.errors.name
+                  ? { ...styles.input_hint, color: theme.SECONDARY_COLOR }
+                  : styles.input_hint
+              }
+            >
+              {formik.errors.name ?? "Class Name"}
             </Text>
-          </View>
-
-          <FieldArray
-            name="cards"
-            render={(arrayHelpers) => {
-              // if (formik.values.cards.length === 0) {
-              //   arrayHelpers.push({});
-              // }
-              fieldArrayRef.current = arrayHelpers;
-              return formik.values.cards.map((item, index) => {
-                let card = `cards.${index}`;
-                return (
-                  <View style={styles.card_block} key={item.id}>
-                    <InputWithImage
-                      type="question"
-                      index={index}
-                      formik={formik}
-                      item={item}
-                      card={card}
-                    />
-                    <InputWithImage
-                      type="answer"
-                      index={index}
-                      formik={formik}
-                      item={item}
-                      card={card}
-                    />
-                  </View>
-                );
-              });
-            }}
-          />
-
-          {/* submit and add card buttons */}
-          <View style={styles.button_group}>
-            <TouchableOpacity
-              disabled={submitting}
-              style={{
-                ...styles.button,
-                borderTopRightRadius: 0,
-                borderBottomRightRadius: 0,
-                flex: 1,
-                marginRight: 10,
-              }}
-              onPress={submitting ? null : formik.handleSubmit}
+            <FastField
+              component={TextInput}
+              style={styles.input}
+              onChangeText={formik.handleChange("description")}
+              onBlur={formik.handleBlur("description")}
+              value={formik.values.description}
+              placeholder="Class Description"
+              placeholderTextColor={theme.TEXT_COLOR_OPACITY}
+              multiline
+            />
+            <Text
+              style={
+                formik.errors.description
+                  ? { ...styles.input_hint, color: theme.SECONDARY_COLOR }
+                  : styles.input_hint
+              }
             >
-              {submitting ? (
-                <ActivityIndicator color={theme.TEXT_COLOR} />
-              ) : (
-                <Text style={styles.button_text}>Submit</Text>
-              )}
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                ...styles.button,
-                borderTopLeftRadius: 0,
-                borderBottomLeftRadius: 0,
-                flex: 3,
-              }}
-              onPress={() => {
-                fieldArrayRef.current.push({ id: latestId.current });
-                latestId.current = latestId.current - 1;
-              }}
-            >
-              <Text style={styles.button_text}>Add Card</Text>
-            </TouchableOpacity>
-          </View>
+              {formik.errors.description ?? "Class Description"}
+            </Text>
+            {/* Cards section */}
+            <View style={styles.header}>
+              <Text style={styles.header_text}>
+                Cards ({formik.values.cards.length})
+              </Text>
+            </View>
 
-          {/* debugging */}
-          <Text style={{ color: theme.TEXT_COLOR }}>
-            {JSON.stringify(formik.errors, null, 2, 0)}
-          </Text>
-        </FormikProvider>
-        <Text style={styles.text}>Class id: {route.params.id}</Text>
-      </View>
-    </KeyboardAwareScrollView>
+            {/* CARDS */}
+            <FieldArray
+              name="cards"
+              render={(arrayHelpers) => {
+                fieldArrayRef.current = arrayHelpers;
+                return formik.values.cards.map((item, index) => {
+                  let card = `cards.${index}`;
+                  return (
+                    <View
+                      style={{
+                        ...styles.card_block,
+                        borderColor:
+                          index % 2 ? theme.TEXT_COLOR : theme.PRIMARY_COLOR,
+                      }}
+                      key={item.id}
+                    >
+                      <InputWithImage
+                        type="question"
+                        index={index}
+                        formik={formik}
+                        item={item}
+                        card={card}
+                      />
+                      <InputWithImage
+                        type="answer"
+                        index={index}
+                        formik={formik}
+                        item={item}
+                        card={card}
+                      />
+                    </View>
+                  );
+                });
+              }}
+            />
+
+            {/* submit and add card buttons */}
+            <View style={styles.button_group}>
+              <TouchableOpacity
+                disabled={submitting}
+                style={{
+                  ...styles.button,
+                  flex: 1,
+                  marginRight: 10,
+                }}
+                onPress={submitting ? null : formik.handleSubmit}
+              >
+                {submitting ? (
+                  <ActivityIndicator color={theme.TEXT_COLOR} />
+                ) : (
+                  <Text style={styles.button_text}>Submit</Text>
+                )}
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  ...styles.button,
+                  borderTopLeftRadius: 0,
+                  borderBottomLeftRadius: 0,
+                  flex: 3,
+                }}
+                onPress={() => {
+                  fieldArrayRef.current.push({ id: latestId.current });
+                  latestId.current = latestId.current - 1;
+                }}
+              >
+                <Text style={styles.button_text}>Add Card</Text>
+              </TouchableOpacity>
+            </View>
+
+            {/* debugging */}
+            {/* <Text style={{ color: theme.TEXT_COLOR }}>
+              {JSON.stringify(formik.errors, null, 2, 0)}
+            </Text> */}
+          </FormikProvider>
+          {/* <Text style={styles.text}>Class id: {route.params.id}</Text> */}
+        </View>
+      </KeyboardAwareScrollView>
+      <FloatingActionButton />
+    </View>
   );
 }
 
@@ -321,11 +328,11 @@ const styles = StyleSheet.create({
     color: theme.TEXT_COLOR,
     padding: 10,
     borderColor: theme.PRIMARY_COLOR,
-    borderWidth: 2,
-    borderRadius: 10,
+    // borderWidth: 0,
+    // borderRadius: 10,
     marginTop: 8,
     width: "100%",
-    backgroundColor: theme.BACKGROUND_COLOR,
+    backgroundColor: theme.BACKGROUND_COLOR_ELEVATED,
   },
   input_hint: {
     color: theme.TEXT_COLOR_OPACITY,
@@ -335,17 +342,18 @@ const styles = StyleSheet.create({
   header: {
     backgroundColor: theme.PRIMARY_COLOR,
     marginTop: 8,
-    marginBottom: 8,
+    marginBottom: 25,
   },
   card_block: {
     marginBottom: 30,
+    backgroundColor: theme.BACKGROUND_COLOR_ELEVATED,
   },
   button_group: {
     flexDirection: "row",
     flex: 1,
     justifyContent: "center",
     // alignItems: "stretch",
-    marginBottom: 30,
+    marginBottom: 90,
   },
   button: {
     backgroundColor: theme.PRIMARY_COLOR,
