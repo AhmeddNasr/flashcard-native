17a02f11d2df7fca148cb89223ff5966aa0ee3a2
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-05-27 08:16:48 +0200

    FEAT: start swipe position determines if answer was correct
    user can swipe from the upper half of the flashcard if he didn't answer correctly, changing the border of the card to red and adding the card to the end of the array.
    
    if the user swipes from the lower half of the flashcard the border will be set green and the card won't be added to the end.

--- ClassScreen.js
+++ ClassScreen.js
@@ -1,7 +1,7 @@
 import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 import * as SQLite from "expo-sqlite";
 import theme from "./theme";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Flashcard from "./Flashcard";
 import { MaterialIcons } from "@expo/vector-icons";
 import FlashcardControl from "./utils/FlashcardControl";
@@ -29,6 +29,7 @@ function ClassScreen({ navigation, route }) {
   const [originalCardData, setOriginalCardData] = useState([]);
   const [ready, setReady] = useState(false);
   const [isEmptyClass, setIsEmptyClass] = useState(false);
+  const [flashcardHeight, setFlashcardHeight] = useState(0);
   const isFocused = useIsFocused();
   const screenWidth = Dimensions.get("screen").width;
   // fetch cards from database
@@ -82,8 +83,8 @@ function ClassScreen({ navigation, route }) {
 
   const slideAnimationSpringConfig = {
     stiffness: 130,
-    damping: 10,
-    mass: 0.5,
+    damping: 11,
+    mass: 0.35,
   };
 
   useEffect(() => {
@@ -125,12 +126,25 @@ function ClassScreen({ navigation, route }) {
     };
   });
 
+  // the starting value of slideBorderColorAnimation is always 0,
+  // if the user swiped from the bottom half of the card the value will decrease down to -1 (translate to a green color)
+  // if the user swiped from the upper half of the card 3 will be added to the value and it will decrease down to 2 (translate to red color)
+  // swiping to a previous card will increase the value either from 3 to +3 or from 0 to 1 (translate to primary color i.e. no color)
   const slideBorderColorAnimationStyle = useAnimatedStyle(() => {
     return {
       borderColor: interpolateColor(
         slideBorderColorAnimation.value,
-        [-1, -0.1, 0],
-        ["#54bd54", "#54bd54", theme.PRIMARY_COLOR]
+        [-1, -0.1, 0, 1, 2, 2.9, 3, 3.1],
+        [
+          theme.GREEN_COLOR,
+          theme.GREEN_COLOR,
+          theme.PRIMARY_COLOR,
+          theme.PRIMARY_COLOR,
+          theme.SECONDARY_COLOR,
+          theme.SECONDARY_COLOR,
+          theme.PRIMARY_COLOR,
+          theme.PRIMARY_COLOR,
+        ]
       ),
     };
   });
@@ -146,38 +160,55 @@ function ClassScreen({ navigation, route }) {
 
   const gestureHandler = useAnimatedGestureHandler(
     {
-      onStart: (_, ctx) => {
+      onStart: (event, ctx) => {
+        // reset value to 0 if it's lower
         if (slideAnimation.value < 0) {
           slideAnimation.value = 0;
-        } else if (slideAnimation.value >= cardData.length - 1) {
+        }
+        // reset value to maximum if it's more
+        else if (slideAnimation.value >= cardData.length - 1) {
           slideAnimation.value = cardData.length - 1;
+        }
+        // if user scrolled upper half set correct to 0 (answer was correct)
+        // if user scrolled lower half set correct to 3 (answer was incorrect)
+        if (event.y / flashcardHeight <= 0.5) {
+          ctx.correct = 3;
+        } else {
+          ctx.correct = 0;
         }
         ctx.startX = slideAnimation.value;
       },
       onActive: (event, ctx) => {
+        // set slideAnimation to ratio of screen swiped + original position
         slideAnimation.value = ctx.startX - event.translationX / screenWidth;
-        slideBorderColorAnimation.value = event.translationX / screenWidth;
+        // set set slideBorderColorAnimation to ratio of screen swiped
+        slideBorderColorAnimation.value =
+          event.translationX / screenWidth + ctx.correct;
       },
       onEnd: (_, ctx) => {
-        // next card
+        // next card if ratio swiped was more than 0.15
         if (slideAnimation.value - ctx.startX > 0.15) {
           slideAnimation.value = withSpring(
             Math.floor(ctx.startX + 1),
-            slideAnimationSpringConfig
+            slideAnimationSpringConfig,
+            // reset border color to primary after swipe animation is complete
+            () => {
+              slideBorderColorAnimation.value = 0;
+            }
           );
-          slideBorderColorAnimation.value = 0;
+          // call incrementFlashcardIndex with true if user swiped from lower half (correct answer), false if not
           if (currentIndex < cardData.length - 1) {
-            runOnJS(incrementFlashcardIndex)(true);
+            runOnJS(incrementFlashcardIndex)(ctx.correct !== 3);
           }
-          // previous card
+          // previous card if ratio swiped was more than 0.15 in negative direction
         } else if (slideAnimation.value - ctx.startX < -0.15) {
           slideAnimation.value = withSpring(
             Math.floor(ctx.startX - 1),
             slideAnimationSpringConfig
           );
-          slideBorderColorAnimation.value = 0;
           runOnJS(decrementFlashcardIndex)();
         } else {
+          // reset card position if swipe was not large enough in either direction
           slideAnimation.value = withSpring(
             Math.floor(ctx.startX),
             slideAnimationSpringConfig
@@ -186,7 +217,13 @@ function ClassScreen({ navigation, route }) {
         }
       },
     },
-    [slideAnimation, cardData, incrementFlashcardIndex, decrementFlashcardIndex]
+    [
+      slideAnimation,
+      cardData,
+      incrementFlashcardIndex,
+      decrementFlashcardIndex,
+      flashcardHeight,
+    ]
   );
 
   if (!ready) {
@@ -217,6 +254,11 @@ function ClassScreen({ navigation, route }) {
               },
               slideAnimationStyle,
             ]}
+            onLayout={(event) => {
+              if (!!!flashcardHeight) {
+                setFlashcardHeight(event.nativeEvent.layout.height);
+              }
+            }}
           >
             {cardData.map((card, index) => (
               <View style={{ marginRight: 20 }} key={index}>
@@ -260,7 +302,7 @@ function ClassScreen({ navigation, route }) {
           </Animated.View>
         </View>
       </View>
-      <View style={styles.controlContainer}>
+      {/* <View style={styles.controlContainer}>
         <FlashcardControl title="Back" action={() => decrementFlashcardIndex()}>
           <MaterialIcons
             name="keyboard-arrow-left"
@@ -302,7 +344,7 @@ function ClassScreen({ navigation, route }) {
             style={styles.controlButton}
           />
         </FlashcardControl>
-      </View>
+      </View> */}
       <Pressable
         style={{ flex: 1, maxHeight: 50 }}
         onPress={() => navigation.navigate("Edit", { id: route.params.id })}
